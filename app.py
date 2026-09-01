83e09791ea041828e82337b00347406616e3c934
s1lly-work <vnuchkahausee@gmail.com>
2026-09-02 01:24:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,17 +41,17 @@ def load_user(user_id):
 
 BUSES_DATA = [
     {"title": "Solaris Urbino 12 III", "image": "8566.png", "category": "u12-iii"},
-    {"title": "Solaris Urbino 12 IV", "image": "8571.png", "category": "u12-iv"},
-    {"title": "Solaris Urbino 18 III", "image": "8658.png", "category": "u18-iii"},
+    {"title": "Solaris Urbino 12 III", "image": "8571.png", "category": "u12-iii"},
+    {"title": "Solaris Urbino 12 III", "image": "8658.png", "category": "u12-iii"},
     {"title": "Solaris Urbino 18 III", "image": "8698.png", "category": "u18-iii"},
-    {"title": "Solaris Urbino 12 III", "image": "8703.png", "category": "u12-iii"},
+    {"title": "Solaris Urbino 18 III", "image": "8703.png", "category": "u18-iii"},
     {"title": "Solaris Urbino 12 III", "image": "8574.png", "category": "u12-iii"},
     {"title": "Solaris Urbino 12 III", "image": "8599.png", "category": "u12-iii"},
-    {"title": "Solaris Urbino 18 III", "image": "8538.png", "category": "u18-iii"},
-    {"title": "Solaris Urbino 12 IV", "image": "8601.png", "category": "u12-iv"},
-    {"title": "Solaris Urbino 12 III", "image": "0231.png", "category": "u12-iii"},
-    {"title": "Solaris Urbino 12 III", "image": "8665.png", "category": "u12-iii"},
-    {"title": "Solaris Urbino 18 III", "image": "8550.png", "category": "u18-iii"},
+    {"title": "Solaris Urbino 12 III", "image": "8538.png", "category": "u12-iii"},
+    {"title": "Solaris Urbino 12 III", "image": "8691.png", "category": "u18-iii"},
+    {"title": "Solaris Urbino 12 IV", "image": "0231.png", "category": "u12-iv"},
+    {"title": "Solaris Urbino 18 III", "image": "8665.png", "category": "u18-iii"},
+    {"title": "Solaris Urbino 12 III", "image": "8550.png", "category": "u12-iii"},
 ]
 
 @app.route('/')
