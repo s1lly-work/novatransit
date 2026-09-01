e4ff398849e5df3fbf1b254aa3f9bf6471d5d579
s1lly-work <vnuchkahausee@gmail.com>
2026-09-01 23:45:00 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -120,6 +120,9 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
