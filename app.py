fad15705d7ffe8fae2477ee2dc031001f2737350
s1lly-work <vnuchkahausee@gmail.com>
2026-09-02 01:26:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nova_transit_secret_key'
